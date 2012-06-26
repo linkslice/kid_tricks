1e7680c77050c61546e9f27762bc459757ec92f7
linkslice <sparctacus@gmail.com>
2012-06-25 18:26:43 -0700

    kinda neat

--- hearts.c
+++ hearts.c
@@ -1,14 +1,41 @@
+#define _BSD_SOURCE
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+void hearts(int);
 
 int main()
 {
 
-	printf("   ***   ***   \n");
-	printf("  ***********  \n");
-	printf("   *********   \n");
-	printf("    *******    \n");
-	printf("     *****     \n");
-        printf("      ***      \n");
-	printf("        *      \n");
+	int count = 0;
+	
+	int p;
+	p = getpid();
+	srand(time(NULL)+p);
+	int y;
+	y = 60 + (int) (100.0 * (rand() / (RAND_MAX + 1.0)));
+
+	hearts(y);
+
+}
+
+
+
+void hearts(int y)
+{
+	while (y--!=0) {	
+
+		printf("   ***   ***   \n");
+		printf("  ***********  \n");
+		printf("   *********   \n");
+		printf("    *******    \n");
+		printf("     *****     \n");
+		printf("      ***      \n");
+		printf("        *      \n");
+	}
 
 }
